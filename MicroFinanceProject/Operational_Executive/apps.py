132041388b1f4e544b03265b76523131e46db42d
anandhm1 <anandmeshram843@gmail.com>
2022-04-10 11:46:15 +0530

    MicroFinanceProject

---
+++ MicroFinanceProject/Operational_Executive/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OperationalExecutiveConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Operational_Executive'
---
+++ MicroFinanceProject/Operational_Head/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OperationalHeadConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Operational_Head'
